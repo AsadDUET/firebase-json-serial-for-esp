f8b573da40e68b09e6ec80b949daa2c7f422d6fc
AsadDUET <asadouzzaman@gmail.com>
2021-05-13 04:39:21 +0600

    all except secrets first commit

---
+++ src/secrets.templet.h
@@ -0,0 +1,10 @@
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "****"
+#define WIFI_PASSWORD "****"
+/* 2. Define the Firebase project host name and API Key */
+#define FIREBASE_PROJECT_HOST "*****.firebaseio.com"
+#define API_KEY "****"
+
+/* 3. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "****"
+#define USER_PASSWORD "****"
